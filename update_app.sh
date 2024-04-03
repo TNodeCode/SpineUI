ddd695e9fe7995cd0ae6c41178825a9ed3aee0c7
Tilo Flasche <muelladresse10@gmail.com>
2024-04-03 10:12:53 +0200

    Update update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -1,5 +1,11 @@
 # Fetch the code from GitHub
 git pull origin main
 
+# Install all dependencies
+pip install -r requirements.txt
+
 # Rebuild docker containers
 bash scripts/docker/build.sh
+
+# Delete old images
+docker system prune -f
