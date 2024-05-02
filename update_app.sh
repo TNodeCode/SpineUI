6059a7c062dc74928ee9010194fd6dacab6e464f
tnodecode <s2.tf@we-code-it.de>
2024-05-02 14:31:11 +0200

    Added mkdocs to CI/CD pipeline

--- update_app.sh
+++ update_app.sh
@@ -5,7 +5,9 @@ git pull origin main
 pip install -r requirements.txt
 
 # Rebuild docker containers
-bash scripts/docker/build.sh
+docker pull tnodecode/spine-nginx
+docker pull tnodecode/spineui
+
 
 # Delete old images
 docker system prune -f
