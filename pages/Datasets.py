72c4d781d64a0eb7b99cc90a2b037ef7c17248b0
tnodecode <s2.tf@we-code-it.de>
2024-10-06 17:23:35 +0200

    Modified Datasets page

--- pages/Datasets.py
+++ pages/Datasets.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import globox
 
 from src.config.datasetconfig import DatasetConfiguration
 
@@ -10,7 +11,12 @@ st.text("On this site you can manage your datasets")
 datasets = []
 for dataset in DatasetConfiguration.get_available_datasets():
     paths = DatasetConfiguration.get_dataset_image_paths(dataset['name'])
-    datasets.append({"name": dataset['name'], "images": len(paths)})
+    annotations = DatasetConfiguration.get_dataset_annotations(dataset['name'])
+    for ann in annotations:
+        if ann['type'] in ['coco', 'mmtracking']:
+            coco: globox.AnnotationSet = globox.AnnotationSet.from_coco(file_path=ann['paths'][0])
+            n_boxes = len(list(coco.all_boxes))
+    datasets.append({"name": dataset['name'], "images": len(paths), "objects": n_boxes})
 
 st.dataframe(pd.DataFrame(datasets))
 
